44c456ea5f692eca247698f3cb6a30227216c1fe
Hiroki Kimiwada <hiroki.kimiwada@masason.org>
2025-01-19 22:10:03 +0900

    Created forward pass in main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from nnfs.datasets import spiral_data
 from dense import Layer_Dense
 from activation_function import ReLu_Activation_Function
 from activation_function import Softmax_Activation_Function
+from cross_entropy_loss import Categorical_CrossEntropy_Loss
 
 nnfs.init()
 X, y = spiral_data(samples=100, classes=3)
@@ -17,13 +18,15 @@ layer_1 = Layer_Dense(2,3)
 relu_1 = ReLu_Activation_Function()
 layer_2 = Layer_Dense(3,3)
 softmax_1 = Softmax_Activation_Function()
+loss_function = Categorical_CrossEntropy_Loss()
 
 # Creating the Neural Network
 layer_1.forward(X)
 relu_1.forward(layer_1.outputs)
 layer_2.forward(relu_1.outputs)
 softmax_1.forward(layer_2.outputs)
-print(f"NN Output: {softmax_1.outputs[:5]}")
+loss = loss_function.calculate(softmax_1.outputs,y)
+print(f"Loss: {loss}")
 
 # Softmax returns 33,333% for each color because the weights are set close to zero (0.01*weights)
     
