9b765d95ca7d6cf89ba4c3a811cc4aa7423329c4
Hiroki Kimiwada <hiroki.kimiwada@masason.org>
2025-01-19 09:34:03 +0900

    Added Comments

--- dense.py
+++ dense.py
@@ -1,4 +1,9 @@
 # Problem statement: the dataset is dots in spiral form. There are several spirals. Each dot in the spiral is one of three colors.
 # Create NN to classify each dot. Eseentially, the input will be two dimensional (X,Y) and output is three dimensional.
 
-# Dense Layer: 
+# Dense Layer Class:
+# Class that, when given number of neurons and number inputs, generates output
+# Class randomly assigns, weights, and biases to each neuron.
+# np.dot(input, Weight) + bias -> Weight (1st column: Weight of neuron 1, 2nd column: Weight of neuron 2 etc...)
+# Weights matrix: inputs x neurons, Bias: neuron number
+
