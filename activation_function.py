ab509c118795d0081df4bea05ea85d258197af71
Hiroki Kimiwada <hiroki.kimiwada@masason.org>
2025-01-19 15:19:10 +0900

    moved around code

--- activation_function.py
+++ activation_function.py
@@ -12,18 +12,18 @@ class ReLu_Activation_Function:
 # Softmax Activation Function: Transform output to probability (0,1)
 # Subtract largest number in row to make sure softmax value isn't too large.
 
-# Testing Softmax 
-input = [[1,2,3,2.5],[2,5,-1,2],[-1.5,2.7,3.3,-0.8]]
-exp_value = np.exp(input - np.max(input,axis=1,keepdims=True))
-prob = exp_value / np.sum(exp_value,axis=1,keepdims=True)
-print(f"Probability from input: {prob}")
-
 # Softmax Activation Class
 class Softmax_Activation_Function:
     def forward(self,inputs):
         exp_value = np.exp(input - np.max(input,axis=1,keepdims=True))
         prob = exp_value / np.sum(exp_value,axis=1,keepdims=True)
         self.outputs = prob
+
+# Testing Softmax 
+input = [[1,2,3,2.5],[2,5,-1,2],[-1.5,2.7,3.3,-0.8]]
+exp_value = np.exp(input - np.max(input,axis=1,keepdims=True))
+prob = exp_value / np.sum(exp_value,axis=1,keepdims=True)
+print(f"Probability from input: {prob}")
 
 # Create Mock NN (Inputs+Neurons+Relu+Neurons+Softmax)
 X, y = spiral_data(samples=100, classes=3)
