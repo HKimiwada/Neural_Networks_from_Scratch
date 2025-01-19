89d378d2b9237426fcf96a169e9bd2bd22b546dd
Hiroki Kimiwada <hiroki.kimiwada@masason.org>
2025-01-19 20:24:59 +0900

    Created numpy calculations for loss

--- cross_entropy_loss.py
+++ cross_entropy_loss.py
@@ -4,4 +4,8 @@ import numpy as np
 
 softmax_outputs = np.array([[0.7,0.1,0.2],[0.1,0.5,0.4],[0.02,0.9,0.08]])
 class_targets = [0,1,1]
-print(softmax_outputs[[0,1,2],class_targets])
+
+logits = softmax_outputs[[0,1,2],class_targets]
+neg_log = -np.log(logits)
+average_loss = np.mean(neg_log)
+print(f"Average Cross Entropy Loss: {average_loss}")
