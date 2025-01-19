6525f4a1f732e1965d121af383c185e0042a4865
Hiroki Kimiwada <hiroki.kimiwada@masason.org>
2025-01-19 15:06:03 +0900

    Creating mock NN

--- activation_function.py
+++ activation_function.py
@@ -1,4 +1,7 @@
 import numpy as np
+import nnfs
+from nnfs.datasets import spiral_data
+from dense import Layer_Dense
 
 # Activation Function: Introduces non-linearity in order to capture any type of function
 class ReLu_Activation_Function: 
@@ -14,3 +17,26 @@ input = [[1,2,3,2.5],[2,5,-1,2],[-1.5,2.7,3.3,-0.8]]
 exp_value = np.exp(input - np.max(input,axis=1,keepdims=True))
 prob = exp_value / np.sum(exp_value,axis=1,keepdims=True)
 print(f"Probability from input: {prob}")
+
+# Softmax Activation Class
+class Softmax_Activation_Function:
+    def forward(self,inputs):
+        exp_value = np.exp(input - np.max(input,axis=1,keepdims=True))
+        prob = exp_value / np.sum(exp_value,axis=1,keepdims=True)
+        self.outputs = prob
+
+# Create Mock NN (Inputs+Neurons+Relu+Neurons+Softmax)
+X, y = spiral_data(samples=100, classes=3)
+
+# Defining the layers
+layer_1 = Layer_Dense(2,3)
+relu_1 = ReLu_Activation_Function()
+layer_2 = Layer_Dense(3,3)
+softmax_1 = Softmax_Activation_Function()
+
+# Creating the Neural Network
+layer_1.forward(X)
+relu_1(layer_1.outputs)
+layer_2(relu_1.outputs)
+softmax_1(layer_2.outputs)
+print(f"NN Output: {softmax_1.output[:5]}")
