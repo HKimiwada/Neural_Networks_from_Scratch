61b3c17ed3129a6bf00d027f6d629c35d9e71832
Hiroki Kimiwada <hiroki.kimiwada@masason.org>
2025-01-19 20:19:38 +0900

    Created loss function python file

---
+++ cross_entropy_loss.py
@@ -0,0 +1,7 @@
+# Coding cross entropy loss = - Sum of P(true) * log(P(actual))
+# Testing cross entropy loss
+import numpy as np
+
+softmax_outputs = np.array([[0.7,0.1,0.2],[0.1,0.5,0.4],[0.02,0.9,0.08]])
+class_targets = [0,1,1]
+print(softmax_outputs[[0,1,2],class_targets])
