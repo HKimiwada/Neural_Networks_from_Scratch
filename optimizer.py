aad440ca412ebc3246e08c0822935bc98e85f2d1
Hiroki Kimiwada <hiroki.kimiwada@masason.org>
2025-01-24 08:46:18 +0900

    Created Gradient Descent Class

--- optimizer.py
+++ optimizer.py
@@ -1,1 +1,9 @@
 # File for Optimizers (Gradient Descent, Adam etc...)
+# Simple Gradien Descent Optimizer
+class Optimizer_GD:
+    def __init__(self, learning_rate=1):
+        self.learning_rate = learning_rate
+    
+    def update_params(self, layer):
+        layer.weights += -self.learning_rate * layer.dweights
+        layer.bias += -self.learning_rate * layer.dbiases
