e10a17b3e736f7ac51b5a9951bde2de14b99111d
Hiroki Kimiwada <hiroki.kimiwada@masason.org>
2025-02-02 11:50:20 +0900

    Completed Adagrad Optimizer

--- optimizer.py
+++ optimizer.py
@@ -70,4 +70,29 @@ class Optimizer_Adagrad:
         self.decay = decay
         self.iterations = 0
         self.epsilon = epsilon
-        
+
+    # Call before parameter updates (for learning rate decay) 
+    def pre_update_params(self):
+        if self.decay:
+            self.current_learning_rate = self.learning_rate / (1 + self.decay * self.iterations)
+    
+    # Update Parameters
+    def update_params(self, layer):
+        # If layer has no cache array (of past graduebts), create one filled with zeros
+        if not hasattr(layer, 'weight_cache'):
+            layer.weight_cache = np.zeros_like(layer.weights)
+            layer.bias_cache = np.zeros_like(layer.biases)
+
+        # Update cache with squared current gradients
+        layer.weight_cache += layer.dweights**2
+        layer.bias_cache += layer.dbiases**2
+
+        # Update weights and biases with adaptive learning rate for each parameter
+        # (learning rate divided by square root of cache + epsilon) -> epsilon is hyperparameter to prevent division by zero
+        layer.weights += -self.current_learning_rate * layer.dweights / (np.sqrt(layer.weight_cache) + self.epsilon)
+        layer.biases += -self.current_learning_rate * layer.dbiases / (np.sqrt(layer.bias_cache) + self.epsilon)
+
+    def post_update_params(self):
+        self.iterations += 1
+
+# RMSprop Optimizer (optimizes learning rate, but with decay to prevent learning rate from getting too small)
