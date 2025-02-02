cacbe941f250f8369776f3fa0e718302a8d5f4e4
Hiroki Kimiwada <hiroki.kimiwada@masason.org>
2025-02-02 11:36:27 +0900

    Created SGD Optimizer with Momentum

--- optimizer.py
+++ optimizer.py
@@ -1,5 +1,7 @@
 # File for Optimizers (Gradient Descent, Adam etc...)
-# Simple Gradien Descent Optimizer (w/learning rate decay) (+add momentum)
+import numpy as np
+
+# Gradient Descent Optimizer (w/learning rate decay) 
 class Optimizer_GD:
     def __init__(self, learning_rate=1, decay=0):
         self.learning_rate = learning_rate
@@ -15,6 +17,47 @@ class Optimizer_GD:
     def update_params(self, layer):
         layer.weights += -self.learning_rate * layer.dweights
         layer.biases += -self.learning_rate * layer.dbiases
+
+    def post_update_params(self):
+        self.iterations += 1
+
+# Optimizer with Momentum
+class Optimizer_SGD:
+    def __init__(self, learning_rate=1, decay=0, momentum=0):
+        self.learning_rate = learning_rate
+        self.current_learning_rate = learning_rate
+        self.decay = decay
+        self.iterations = 0
+        self.momentum = momentum
+    
+    # Call before any parameter updates (to do learning rate decay)
+    def pre_update_params(self):
+        if self.decay:
+            self.current_learning_rate = self.learning_rate / (1 + self.decay * self.iterations)
+
+    def update_params(self, layer):
+        if self.momentum:
+            # if no momentum arrays, create momentum array with zeros
+            if not hasattr(layer, 'weight_momentums'):
+                layer.weight_momentums = np.zeros_like(layer.weights)
+                layer.bias_momentums = np.zeros_like(layer.biases)
+            
+            # Weight updates with momentum. Takes previous weight updates multiplied by some hyperparameter factor
+            # and adds to current gradient update
+            weight_updates = self.momentum * layer.weight_momentums - self.current_learning_rate * layer.dweights
+            layer.weight_momentums = weight_updates # Update momentum array
+
+            # Build bias updates
+            bias_updates = self.momentum * layer.bias_momentums - self.current_learning_rate * layer.dbiases
+            layer.bias_momentums = bias_updates # Update momentum array
+
+        else: # No momentum so Simple Gradient Descent
+            weight_updates = -self.current_learning_rate * layer.dweights
+            bias_updates = -self.current_learning_rate * layer.dbiases
+        
+        # update weights and biases with either updates from momentum or simple gradient descent
+        layer.weights += weight_updates
+        layer.biases += bias_updates
 
     def post_update_params(self):
         self.iterations += 1
