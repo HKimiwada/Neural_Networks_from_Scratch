afec1484d3de11a6f355ff86f7f96e638ebcebe5
Hiroki Kimiwada <hiroki.kimiwada@masason.org>
2025-02-02 11:57:19 +0900

    Created RMSprop Optimizer

--- optimizer.py
+++ optimizer.py
@@ -96,3 +96,34 @@ class Optimizer_Adagrad:
         self.iterations += 1
 
 # RMSprop Optimizer (optimizes learning rate, but with decay to prevent learning rate from getting too small)
+class Optimizer_RMSprop:
+    def __init__(self, learning_rate=0.001, decay=0, epsilon=1e-7, rho=0.9):
+        self.learning_rate = learning_rate
+        self.current_learning_rate = learning_rate
+        self.decay = decay
+        self.iterations = 0
+        self.epsilon = epsilon
+        self.rho = rho
+    
+    # Call before any parameter updates (to do learning rate decay)
+    def pre_update_params(self):
+        if self.decay:
+            self.current_learning_rate = self.learning_rate / (1 + self.decay * self.iterations)
+    
+    # Update Parameters
+    def update_params(self, layer):
+        # If layer does not contain cache arrays, create them filled with zeros.
+        if not hasattr(layer, 'weight_cache'):
+            layer.weight_cache = np.zeros_like(layer.weights)
+            layer.bias_cache = np.zeros_like(layer.biases)
+        
+        # Update cache with squared + decayed gradients
+        layer.weight_cache = self.rho * layer.weight_cache + (1 - self.rho) * layer.dweights**2
+        layer.bias_cache = self.rho * layer.bias_cache + (1 - self.rho) * layer.dbiases**2
+
+        # Vanilla SGD update with adaptive learning rate + normalization
+        layer.weights += -self.current_learning_rate * layer.dweights / (np.sqrt(layer.weight_cache) + self.epsilon)
+        layer.biases += -self.current_learning_rate * layer.dbiases / (np.sqrt(layer.bias_cache) + self.epsilon)
+    
+    def post_update_params(self):
+        self.iterations += 1
