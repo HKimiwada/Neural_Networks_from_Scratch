432595e8c05431e3bce55785b727e4b01d64af82
Hiroki Kimiwada <hiroki.kimiwada@masason.org>
2025-02-03 10:54:47 +0900

    Updated Code

--- CNN/main.py
+++ CNN/main.py
@@ -1,4 +1,10 @@
 # main file for implementing CNN model from scratch
+import sys
+import os
+
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.insert(0, project_root)
+
 from MLP.dense import Layer_Dense
 from convolutional import Convolutional
 
